797700f7d67b16302e519b81ce96b87366e68bc2
15720301013 <877213524@qq.com>
2016-11-28 21:06:06 +0800

    Text

---
+++ myText/js/index.js
@@ -0,0 +1,30 @@
+$(function(){	
+	console.log(new Date());	
+	var index=1;
+	var timer;  //定时器
+	$("img:first").show();     //展示图片第一个
+	$("ol li:first").css("background-color","brown"); //索引收个设置样式
+//获取当前经过的索引值，显示对应的图片和样式
+$("li").mouseover(function(){      
+		index=$("li").index(this);
+		change(index);
+	});
+//获取盒子 注入鼠标经过事件
+$("#run").hover(function(){//鼠标经过时停止动画
+	clearInterval(timer);       
+},function(){      //鼠标经过时开始动画
+	timer=setInterval(function(){
+		console.log(index+new Date());	
+		change(index)
+		index++;
+		if(index==4){
+			index=0;
+		}
+	},2000);
+	
+}).trigger("mouseleave");    //选定run 触发鼠标移开事件
+})
+function change(index){
+		$(" ol li").eq(index).css("background-color","brown").siblings().css("background-color","")
+		$("img").eq(index).fadeIn().siblings().fadeOut();
+}
